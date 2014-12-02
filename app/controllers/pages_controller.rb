3b4449c3408ac9a7d2a56c847959c870eacf03e0
McCartney Bohn <mccartneybohn@yahoo.com>
2014-12-02 11:39:57 -0700

    added rspec test and also added render forms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,16 @@
 class PagesController < ApplicationController
+before_action :authorize, only: :show
+
+  def index
+    authorize
+  end
+
+  private
+
+  def authorize
+    if session[:user_id] != 3
+      raise AccessDenied
+    end
+  end
 
 end
