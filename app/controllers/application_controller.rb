d06987e5228b2feebd373486c98b74a084be08c4
McCartney Bohn <mccartneybohn@yahoo.com>
2014-12-02 10:39:20 -0700

    added 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  class AccessDenied < StandardError
+  end
+
+  rescue_from AccessDenied, with: :foobar
+
+  def foobar
+    render file: "public/404", layout: false, status: 404
+  end
 end
